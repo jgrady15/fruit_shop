723c9d609987ad1188082b193e4d0af9e6af087a
Jonathan Grady Ong <gradyjonathan55@gmail.com>
2022-10-01 09:15:17 -0700

    linked sanity API and sanity token
    created private key token in styles/.env

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import React from "react";
 
-const Home = () => {
+import { Client } from '../lib/client';
+
+const Home = ({ products, bannerData }) => {
     return (
         <div>
             <div className="products-heading">
                 <h2>Best Selling Fruits!</h2>
             </div>
+
+            <div className="products-container">
+                {products?.map((product) => product.name)}
+            </div>
         </div>
     )
+}
 
+export const getServerSideProps = async () => {
+    const productQuery = '*[_type == "product"]';
+    const products = await Client.fetch(productQuery);
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await Client.fetch(bannerQuery);
+
+    return {
+        props: { products, bannerData }
+    }
 }
 
 export default Home;
